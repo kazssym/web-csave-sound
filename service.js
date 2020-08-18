0a548413732fdc8fcb2d4f1b03367060f71dc492
Kaz Nishimura <kazssym@linuxfront.com>
2020-08-18 15:06:35 +0900

    Add the default page to the cache

--- service.js
+++ service.js
@@ -30,6 +30,7 @@ self.addEventListener("install", (event) => {
     event.waitUntil(caches.open(CACHE_NAME)
         .then((cache) => {
             return cache.addAll([
+                "./",
                 "index.html",
                 "app.webmanifest",
                 "service.js",
